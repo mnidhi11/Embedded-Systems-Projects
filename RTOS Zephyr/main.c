73d13aed83110848bfa4722cd6bbc488682f0d9d
mnidhi11 <mnidhi1195@gmail.com>
2020-09-07 06:11:48 -0700

    Update main.c

--- RTOS Zephyr/main.c
+++ RTOS Zephyr/main.c
@@ -1,129 +1,140 @@
 /*
- * Copyright (c) 2016 Intel Corporation
+ * Copyright (c) 2016 Open-RnD Sp. z o.o.
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "hcsr04.h"
 #include <zephyr.h>
+//#include <board.h>
 #include <version.h>
 #include <device.h>
-#include <i2c.h>
+#include <sensor.h>
 #include <gpio.h>
 #include <stdio.h>
 #include <pwm.h>
-#include <kernel.h>
-#include <sensor.h>
+#include <misc/util.h>
+#include <misc/printk.h>
 #include <pinmux.h>
 #include "../boards/x86/galileo/board.h"
 #include "../boards/x86/galileo/pinmux_galileo.h"
 #include "../drivers/gpio/gpio_dw_registers.h"
 #include "../drivers/gpio/gpio_dw.h"
+#include <shell/shell.h>
 #include <inttypes.h>
-#include <misc/util.h>
-#include <misc/printk.h>
-#include <misc/__assert.h>
-#include <logging/log.h>
-static int ret;
+#include <stdlib.h>
 #define EDGE_FALLING    (GPIO_INT_EDGE | GPIO_INT_ACTIVE_LOW)
 #define EDGE_RISING	(GPIO_INT_EDGE | GPIO_INT_ACTIVE_HIGH)
 #define PULL_UP 	0	/* change this to enable pull-up/pull-down */
 #define SLEEP_TIME	1000	/* Sleep time */
-#define LOG_LEVEL CONFIG_SENSOR_LOG_LEVEL
-int count_1=0;
-//static struct gpio_callback gpio_cb;
-LOG_MODULE_REGISTER(HCSR04);
+struct device *dev_cmd,*dev_cmd_1;
+struct k_sem my_sem, my_sem_1;
+struct k_sem c_sem;
+struct k_sem d_sem;
+int user_count;
+char selected_sensor;
+u32_t time1,time2;
+K_SEM_DEFINE(my_sem, 1, 1);
+K_SEM_DEFINE(my_sem_1, 0, 1);
+K_SEM_DEFINE(c_sem, 0, 1); // high prioity thread's semaphore is initially not available
+K_SEM_DEFINE(d_sem, 1, 1); // low prioity thread's semaphore is initially available
+struct device  *pwm0;
+struct device *gpiob_1;
+struct measurements{ 
+        u32_t measured_distance;
+        u32_t measured_timestamp;
+        };
+struct measurements value[256];
+int distance_count = 0;
+int count = 0;
+static struct gpio_callback gpio_cb;
 
-static struct device *pinmux;
+
+static int cmd_select(const struct shell *shell, size_t argc, char **argv){
+    printk("the number of arguments passed is %zd\n",argc);
+    printk("the value of argument is %s\n",argv[1]);
+    if (atoi(argv[1])==0){
+        printk("No sensor is enabled \n");
+    }
+    else if (atoi(argv[1])==1){
+        printk("Sensor 1 selected \n");
+        dev_cmd = device_get_binding("HCSRO4_1");
+    }
+    else if (atoi(argv[1])==2){
+        printk("Sensor 2 selected \n");
+        dev_cmd = device_get_binding("HCSRO4_2");
+    }
+    else{
+        printk("invalid sensor\n");
+    }
+   
+    
+
+    if (dev_cmd == NULL) {
+		printk("Could not get sensor device\n");
+		return 0;
+    }
+ 
+    printk("dev %p name %s\n", dev_cmd, dev_cmd->config->name);
+
+    sensor_sample_fetch(dev_cmd);
+    sensor_channel_get(dev_cmd, SENSOR_CHAN_DISTANCE, &val);
+    return 0;
+}
+
+static int cmd_start(const struct shell *shell, size_t argc, char **argv){
+    if((atoi(argv[1]))>256){
+        printk("The number of measurement should be less than 256\n"); 
+        
+    }
+    else{
+        user_count=atoi(argv[1]);
+        
+        struct sensor_value val;
+        for (int i=0;i<atoi(argv[1]);i++){ 
+            printk("while loop entered\n");
+            sensor_sample_fetch(dev_cmd);  
+            value[i].measured_distance=sensor_channel_get(dev_cmd, SENSOR_CHAN_DISTANCE, &val);
+            value[i].measured_timestamp=SYS_CLOCK_HW_CYCLES_TO_NS(k_cycle_get_32());
+            sensor_sample_fetch(dev_cmd);
+            k_sleep(500); 
+       
+        } 
+    printk("Distance measurement over\n");
+    }
+
+    return 0;
 
 
+}
 
-static int hcsr04_sample_fetch(struct device *dev, enum sensor_channel chan)
-{
-	printk("Sample fetch called");
-	return 0;
+static int cmd_dump(const struct shell *shell, size_t argc, char **argv){
+    ARG_UNUSED(argc);
+    ARG_UNUSED(argv);
+    for (int i=0;i<user_count;i++){
+        printk("Distance %d: %"PRIu32" centimeters, Timestamp: %"PRIu32"\n",i+1, value[i].measured_distance,value[i].measured_timestamp);
+    }
+    return 0;
 }
 
 
 
-static int hcsr04_channel_get(struct device *dev,enum sensor_channel chan,struct sensor_value *val)
-{
-	printk("channel get called");
-	return 0;
-}
+SHELL_STATIC_SUBCMD_SET_CREATE(sub_demo,
+        SHELL_CMD_ARG(Select_HCSR,NULL,"select.",cmd_select,2,0),
+        SHELL_CMD_ARG(Start_HCSR,NULL,"start.",cmd_start,2,0),
+        SHELL_CMD_ARG(Dump_HCSR,NULL,"dump.",cmd_dump,1,0),
+	SHELL_SUBCMD_SET_END /* Array terminated. */
+);
 
-static int hcsr04_init(struct device *dev)
+
+SHELL_CMD_REGISTER(demo, &sub_demo, "Demo commands", NULL); //parent shell command demo
+
+
+
+static struct device *pinmux;  
+
+void main(void)
 {
 	
-	printk("Init called");
-	pinmux=device_get_binding(CONFIG_PINMUX_NAME);
-	struct hcsr04_data *drv_data = dev->driver_data;
-
-	struct galileo_data *dev_1 = pinmux->driver_data;
-
-	if (strcmp(dev->config->name,"HCSR04_1")==0)
-	{
-
-	printk("HCSR04_1 initialized");
-	drv_data->trigger_galileo = CONFIG_Trigger_g_1;
-	drv_data->echo_galileo= CONFIG_Echo_g_1;
-	drv_data->trigger_zephyr=CONFIG_Trigger_z_1;
-	drv_data->echo_zephyr = CONFIG_Echo_z_1;
-	drv_data->gpio=dev_1->gpio_dw;
-    if (!drv_data->gpio) {
-        printk("error for gpio_dw for sensor1\n");
-        return 0;
-    }
-
-	ret=pinmux_pin_set(pinmux,drv_data->trigger_galileo,PINMUX_FUNC_A);
-	if(ret<0)
-	printk("Error for IO1\n");
-    ret=pinmux_pin_set(pinmux,drv_data->echo_galileo,PINMUX_FUNC_B); 	//IO3 for echo i.e. handling interrupt for sensor 1
-    if(ret<0)
-	printk("Error for IO3\n");
-
-	}
-
-
-	else if (strcmp(dev->config->name,"HCSR04_2")==0)
-	{
-	printk("HCSR04_2 initialized");
-	drv_data->trigger_galileo = CONFIG_Trigger_g_2;
-	drv_data->echo_galileo= CONFIG_Echo_g_2;
-	drv_data->trigger_zephyr=CONFIG_Trigger_z_2;
-	drv_data->echo_zephyr = CONFIG_Echo_z_2;
-	drv_data->gpio=dev_1->gpio_dw;
-    if (!drv_data->gpio) {
-        printk("error for gpio_dw for sensor2\n");
-        return 0;
-    }
-	ret=pinmux_pin_set(pinmux,drv_data->trigger_galileo,PINMUX_FUNC_A);
-	if(ret<0)
-	printk("Error for IO0\n");
-    ret=pinmux_pin_set(pinmux,drv_data->echo_galileo,PINMUX_FUNC_B); 	//IO3 for echo i.e. handling interrupt for sensor 1
-    if(ret<0)
-	printk("Error for IO2\n");
-
-	}
-
+        printk("passing over to shell\n");
 
 }
-
-
-
-static const struct sensor_driver_api hcsr04_driver_api = {
-	.sample_fetch = hcsr04_sample_fetch,
-	.channel_get = hcsr04_channel_get,
-};
-
-
-static struct hcsr04_data hcsr04_data1;
-
-DEVICE_AND_API_INIT(hcsr04_sen1, "HCSR04_1",hcsr04_init, &hcsr04_data1,
-		    NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,
-		    &hcsr04_driver_api);
-
-
-/*DEVICE_AND_API_INIT(hcsr04_sen2 "HCSR04_2",hcsr04_init, &hcsr004_data,
-		    NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY,
-		    &hcsr04_driver_api);*/
