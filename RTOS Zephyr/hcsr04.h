a15a8f917c2be8d0c5faec0a2f492f8fcab570bf
mnidhi11 <mnidhi1195@gmail.com>
2020-11-12 12:26:21 -0700

    Update hcsr04.h
    Modified the structure member gpio_cb

--- RTOS Zephyr/hcsr04.h
+++ RTOS Zephyr/hcsr04.h
@@ -1,23 +1,5 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#ifndef ZEPHYR_DRIVERS_SENSOR_HDC1008_HDC1008_H_
-#define ZEPHYR_DRIVERS_SENSOR_HDC1008_HDC1008_H_
-
 #include <kernel.h>
 
-#define HDC1008_I2C_ADDRESS	0x40
-
-#define HDC1008_REG_TEMP	0x0
-#define HDC1008_REG_HUMIDITY	0x1
-#define HDC1008_REG_MANUFID	0xFE
-#define HDC1008_REG_DEVICEID	0xFF
-
-#define HDC1008_MANUFID		0x5449
-#define HDC1008_DEVICEID	0x1000
 
 struct hcsr04_data {
 	struct device *gpio;
@@ -33,4 +15,3 @@ struct hcsr04_data {
 	struct k_sem data_sem;
 };
 
-#endif
